75933ace803f422491604aa48602dbe990df6fa9
ZhefengJin <wugis1219@gmail.com>
2025-06-10 16:52:48 +0900

    update testExportOneMVTTile.ts

--- src/testExportOneMVTTile.ts
+++ src/testExportOneMVTTile.ts
@@ -1,3 +1,4 @@
+import { Buffer } from "buffer";
 import Protobuf from "pbf";
 import { VectorTile } from "@mapbox/vector-tile";
 import exportOneMVTTile from "./lib/exportOneMVTTile";
@@ -20,7 +21,7 @@ console.log(featureCollection);
 
 exportOneMVTTile(tilingOptions, featureCollection, 1, 1, 1);
 
-const buffer = generateMVTTileBuffer(featureCollection.features, {
+const uint8Array = generateMVTTileBuffer(featureCollection.features, {
     x: 1,
     y: 1,
     level: 1,
@@ -29,7 +30,37 @@ const buffer = generateMVTTileBuffer(featureCollection.features, {
     tileExtent: 4096
 });
 
-const vectorTile = new VectorTile(new Protobuf(buffer));
+console.log("buffer");
+console.log(Array.from(uint8Array));
+
+const buffer = Buffer.from(uint8Array);
+
+const base64String = buffer.toString("base64");
+
+console.log("base64String");
+console.log(base64String);
+
+function base64ToUint8Array(base64String: string) {
+    const binaryString = Buffer.from(base64String, "base64").toString("binary");
+    const uint8Array = new Uint8Array(binaryString.length);
+
+    for (let i = 0; i < binaryString.length; i++) {
+        uint8Array[i] = binaryString.charCodeAt(i);
+    }
+
+    return uint8Array;
+}
+
+const base64StringFromBq =
+    "GqICCgR0ZXN0Eh0SEgAAAQACAQMCBAMFBAYFBwYLBxgBIgUJlAaUEBoDRklEGghPQkpFQ1RJRBoJQ0lUWV9OQU1FGglHTUlfQURNSU4aCkFETUlOX05BTUUaCkZJUFNfQ05UUlkaCkNOVFJZX05BTUUaBlNUQVRVUxoDUE9QGghQT1BfUkFOSxoJUE9QX0NMQVNTGgdQT1JUX0lEGgpMQUJFTF9GTEFHGgpQT1BfU09VUkNFIgkZAAAAAADOo0AiCAoGTmVsc29uIgkKB05aTC1OTVIiFAoSTmVsc29uLU1hcmxib3JvdWdoIgQKAk5aIg0KC05ldyBaZWFsYW5kIhQKElByb3ZpbmNpYWwgY2FwaXRhbCIJGQAAAABA/+pAKIAgeAI=";
+
+const bufferFromStringFromBrowser =
+    "GqICCgR0ZXN0Eh0SEgAAAQACAQMCBAMFBAYFBwYLBxgBIgUJzD2UEBoDRklEGghPQkpFQ1RJRBoJQ0lUWV9OQU1FGglHTUlfQURNSU4aCkFETUlOX05BTUUaCkZJUFNfQ05UUlkaCkNOVFJZX05BTUUaBlNUQVRVUxoDUE9QGghQT1BfUkFOSxoJUE9QX0NMQVNTGgdQT1JUX0lEGgpMQUJFTF9GTEFHGgpQT1BfU09VUkNFIgkZAAAAAADOo0AiCAoGTmVsc29uIgkKB05aTC1OTVIiFAoSTmVsc29uLU1hcmxib3JvdWdoIgQKAk5aIg0KC05ldyBaZWFsYW5kIhQKElByb3ZpbmNpYWwgY2FwaXRhbCIJGQAAAABA/+pAKIAgeAI=";
+
+const bufferFromString = base64ToUint8Array(base64StringFromBq);
+
+// const vectorTile = new VectorTile(new Protobuf(uint8Array));
+const vectorTile = new VectorTile(new Protobuf(bufferFromString));
 
 Object.values(vectorTile.layers).forEach((layer: any) => {
     for (let i = 0; i < layer.length; i++) {
